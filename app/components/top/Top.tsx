edec3f34176f6927ca506d6a4c6781d958638099
KNSN92 <kekenshin92@gmail.com>
2025-02-25 18:15:11 +0900

    トップのアイコンのcssを修正。

--- app/components/top/Top.tsx
+++ app/components/top/Top.tsx
@@ -6,13 +6,15 @@ export default function Profile({ className }: { className?: string }) {
   return (
     <div className={`w-screen h-screen flex items-center ${className}`}>
       <div className="flex flex-col items-center mx-auto">
-        <Image
-          src={`${BASE_PATH}/icon.png`}
-          alt="My Icon"
-          width={192}
-          height={192}
-          className="rounded-full border-gray-300 border-solid border w-fit"
-        />
+        <div className="relative w-64 h-64">
+          <Image
+            src={`${BASE_PATH}/icon.png`}
+            alt="My Icon"
+            fill={true}
+            objectFit="contain"
+            className="rounded-full border-gray-300 border-solid border"
+          />
+        </div>
         <p className="w-fit mt-4 text-4xl text-center font-normal text-gray-500 lg:text-4xl dark:text-gray-400">
           Kenshin Nakao
         </p>
