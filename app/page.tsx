cdeb128c61caca78867a59a2222d3e21404aee2b
KNSN92 <kekenshin92@gmail.com>
2025-11-04 13:47:25 +0900

    add: このポートフォリオ古いから新しい方見てね〜を追加

--- app/page.tsx
+++ app/page.tsx
@@ -31,6 +31,27 @@ export default function Home() {
 
   return (
     <div className="overflow-hidden bg-black">
+      <a
+        href="https://knsn92.github.io/"
+        className="fixed z-10 top-0 w-full p-2 bg-red-700 opacity-80 flex items-center gap-2 text-sm lg:text-2xl"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-12"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z"
+          />
+        </svg>
+        お知らせ : このポートフォリオはもう更新されません。
+        新しいポートフォリオをご覧下さい。
+      </a>
       <Top className="scrollSection" />
       <Profile className="scrollSection" />
       <Skills />
